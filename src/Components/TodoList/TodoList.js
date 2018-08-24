c23a31f9f3cfa87f3aedc8f365a9f499760c508f
Artech Chu <finalclassic@outlook.com>
2018-08-24 15:51:29 +0800

    redux demo:add item function finish

--- src/Components/TodoList/TodoList.js
+++ src/Components/TodoList/TodoList.js
@@ -1,27 +1,52 @@
 import React, { Component } from 'react';
 import { Input, Button, List } from 'antd';
-const data = [
-    'Racing car sprays burning fuel into crowd.',
-    'Japanese princess to wed commoner.',
-    'Australian walks 100km after outback crash.',
-    'Man charged over missing wedding girl.',
-    'Los Angeles battles huge wildfires.',
-];
+import store from '../../store/index';
 
 class TodoList extends Component {
+
+    constructor(props){
+        super(props);
+        this.state = store.getState();
+        store.subscribe(()=>this.onStoreChanged());
+    }
+
     render() {
         return (
             <div>
-                <Input placeholder="请输入item" style={{ width: "400px" }} />
-                <Button type="primary">确定</Button>
+                <Input 
+                    placeholder="请输入item" 
+                    style={{ width: "400px" }} 
+                    defaultValue={this.state.inputValue} 
+                    onChange={(e)=>this.onInputChanging(e)}
+                />
+                <Button type="primary" onClick={()=>this.onAddButtonClicked()}>确定</Button>
                 <List
                     bordered
-                    dataSource={data}
+                    dataSource={this.state.list}
                     renderItem={item => (<List.Item>{item}</List.Item>)}
                     style={{marginTop:"10px", width:"400px"}}
                 />
             </div>
         );
+    }
+
+    onInputChanging=(e)=>{ //告诉ActionCreator要干什么事情
+        const action = {
+            type:"change_input_value", //要干的事情
+            value:e.target.value //需要传递的值
+        }
+        store.dispatch(action);
+    }
+
+    onStoreChanged = ()=>{
+        this.setState(store.getState());
+    }
+
+    onAddButtonClicked = ()=>{
+        let action = {
+            type:'add_todoItem',
+        };
+        store.dispatch(action);
     }
 }
 
---
+++ src/store/reducer.js
@@ -0,0 +1,22 @@
+const defaultState = {
+    inputValue:'',
+    list:[1,2,3]
+};
+
+export default (state = defaultState, action) => { //action表示用户传过来的那句话，state表示store上一次存储数据
+    if(action.type==='change_input_value'){
+        //reducer的一个缺陷：reducer可以接收state，但不能修改state，所以这里要用return newState
+        const newState = JSON.parse(JSON.stringify(state));//深拷贝
+        newState.inputValue = action.value;
+        return newState;
+    }
+    if(action.type==='add_todoItem'){
+        const newState = JSON.parse(JSON.stringify(state));
+        newState.list.push(newState.inputValue);
+        newState.inputValue = '';
+        console.log(newState);
+        return newState;
+    }
+    
+    return state;
+}
