30e648764a806b268b5437eee950da0fce6aa4dc
Artech Chu <finalclassic@outlook.com>
2018-08-24 14:28:25 +0800

    install antd and init view

--- src/Components/TodoList/TodoList.js
+++ src/Components/TodoList/TodoList.js
@@ -1,14 +1,32 @@
 import React, { Component } from 'react';
+import { Input, Button, List } from 'antd';
+
 class TodoList extends Component {
 
 
     render() {
+        var data = [
+            'Racing car sprays burning fuel into crowd.',
+            'Japanese princess to wed commoner.',
+            'Australian walks 100km after outback crash.',
+            'Man charged over missing wedding girl.',
+            'Los Angeles battles huge wildfires.',
+        ];
+        
         return (
-           <div>hell React</div>
+            <div>
+                <Input placeholder="请输入item" style={{ width: "400px" }} />
+                <Button type="primary">确定</Button>
+                <List
+                    bordered
+                    dataSource={data}
+                    renderItem={item => (<List.Item>{item}</List.Item>)}
+                />
+            </div>
         );
     }
 
-  
+
 }
 
 export default TodoList;
