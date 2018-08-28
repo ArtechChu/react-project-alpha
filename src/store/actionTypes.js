98dcd4373559d1c40b07897be1db091310f85de0
Artech Chu <finalclassic@outlook.com>
2018-08-28 15:12:07 +0800

    init project for react-redux：delete files

--- src/store/actionTypes.js
+++
@@ -1,7 +0,0 @@
-const actionType = {
-    CHANGE_INPUT_VALUE: 'change_input_value',
-    ADD_TODOITEM: 'add_todoItem',
-    DELETE_TODOITEM: 'delete_todoItem',
-    Init_TODOITEM:'init_todoItem'
-}
-export default actionType;
