16e202fc2aa84023fbc6d68e60b10d3f7663322f
Artech Chu <finalclassic@outlook.com>
2018-08-28 17:02:40 +0800

    补充注释

--- src/TodoList.js
+++ src/TodoList.js
@@ -29,7 +29,7 @@ const mapStateToProps = (state) => {
         list: state.list
     };
 }
-//store.dispatch方法挂到了
+//store.dispatch方法挂载到了这个方法中
 const mapDispatchToProps = (dispatch) => {
     return {
         changeInputValue(e) {
@@ -47,6 +47,8 @@ const mapDispatchToProps = (dispatch) => {
     }
 }
 //connect 表示将 TodoList同store做连接
-//怎么做连接？（即连接的规则）：在 mapStateToProps中
-//如果想对store中的数据做修改
-export default connect(mapStateToProps, mapDispatchToProps)(TodoList);
+//怎么做连接？（即连接的规则）：在 mapStateToProps中 进行映射
+//如果想对store中的数据做修改，则通过 mapDispatchToProps 来进行
+//connect执行返回的结果其实是一个容器组件，把相关的业务逻辑全部都整合到了UI组件中了。
+//导出的其实是一个容器组件
+export default connect(mapStateToProps, mapDispatchToProps)(TodoList); 
